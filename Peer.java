030ec250c7378297f2d06c16ec1640a6d91ca3be
Gautam Agrawal <32679872+gautam0998@users.noreply.github.com>
2021-02-28 21:12:16 +0530

    Parameter Descriptions

--- Peer.java
+++ Peer.java
@@ -12,3 +12,21 @@ public class Peer {
     private Socket hostSocket;
 
 }
+
+/**
+ * @param peerID Special Peer ID assigned to each Peer
+ * 
+ * @param hostName The Hostname given to each host
+ * 
+ * @param portNumber The port number used for connection
+ * 
+ * @param isFile Boolean value to see if the peer has the file or not
+ * 
+ * @param bitField To show which all pieces are present
+ * 
+ * @param chocked Boolean value to determine if the peer is chocked or unchocked
+ * 
+ * @param downloadSpeed To determine the download speed of peer
+ * 
+ * @param hostSocket The socket of host to be connected to
+ */
