7e623a261ac79c8951fa608c3a485d3bdd6d860b
Gautam Agrawal <32679872+gautam0998@users.noreply.github.com>
2021-03-01 12:40:39 +0530

    Get and Set Functions

--- Peer.java
+++ Peer.java
@@ -7,10 +7,9 @@ public class Peer {
     private int portNumber;
     private boolean isFile;
     private byte[] bitField;
-    private boolean chocked;
+    private boolean choked;
     private long downloadSpeed;
     private Socket hostSocket;
-
 }
 
 /**
@@ -24,9 +23,127 @@ public class Peer {
  * 
  * @param bitField To show which all pieces are present
  * 
- * @param chocked Boolean value to determine if the peer is chocked or unchocked
+ * @param choked Boolean value to determine if the peer is chocked or unchoked
  * 
  * @param downloadSpeed To determine the download speed of peer
  * 
  * @param hostSocket The socket of host to be connected to
  */
+
+ /**
+  * Constructor
+  */
+ public Peer() {
+
+ }
+
+ /**
+  * Setting and getting values
+  */
+
+  /**
+   * peerID
+   */
+
+  public void setpeerID(int peerID) {
+      this.peerID = peerID;
+  }
+
+  public void getpeerID() {
+      return peerID;
+  }
+
+
+  /**
+   * hostName
+   */
+
+public void sethostName(String hostName) {
+    this.hostName = hostName;
+}
+
+public String gethostName() {
+    return hostName;
+}
+
+
+/**
+ * portNumber
+ */
+
+ public void setportNumber(int portNumber) {
+     this.portNumber = portNumber;
+ }
+
+ public int getportNumber() {
+     return portNumber;
+ }
+
+
+ /**
+  * isFile
+  */
+
+  public void setisFile(boolean isFile) {
+      this.isFile = isFile;
+  }
+
+  public void setisFile(int isFile) {
+      this.isFile = Integer.parseInt(isFile)
+  }
+
+  public boolean getisFile() {
+      return isFile;
+  }
+
+
+  /**
+   * bitField
+   */
+
+   public void setbitField (byte[] bitField) {
+       this.bitField = bitField;
+   }
+
+   public byte[] getbitField () {
+       return bitField;
+   }
+
+
+   /**
+    * Choked
+    */
+
+    public void setchoked (boolean choked) {
+        this.choked = choked;
+    }
+
+    public boolean getchoked () {
+        return choked;
+    }
+
+
+    /**
+     * downloadSpeed
+     */
+
+     public void setdownloadSpeed (long downloadSpeed) {
+         this.downloadSpeed = downloadSpeed;
+     }
+
+     public long getdownloadSpeed () {
+         return downloadSpeed;
+     }
+
+
+     /**
+      * hostSocket
+      */
+
+      public void sethostSocket (Socket hostSocket) {
+          this.hostSocket = hostSocket
+      }
+
+      public Socket gethostSocket () {
+          return hostSocket;
+      }
