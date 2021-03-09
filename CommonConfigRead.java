16faf11fc87f635e0f17e3fdbe8fbee3ea2ff3d6
Gautam Agrawal <32679872+gautam0998@users.noreply.github.com>
2021-03-09 13:26:02 +0530

    File variables

---
+++ CommonConfigRead.java
@@ -0,0 +1,16 @@
+/*
+Code to read properties from config file
+*/
+
+public class CommonConfigRead {
+
+  public static final string common_file_name = "common.config";
+  private static int NumberOfPreferredNeighbors;
+  private static int  UnchokingInterval;
+  private static int  OptimisticUnchokingInterval;
+  private static string FileName;
+  private static int FileSize;
+  private static int PieceSize;
+
+
+}
