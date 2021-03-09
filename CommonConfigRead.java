0db244fb9e8516c5d902920da9e9d1a02e51deee
Gautam Agrawal <32679872+gautam0998@users.noreply.github.com>
2021-03-09 13:32:20 +0530

    Setters and Getters for variables

--- CommonConfigRead.java
+++ CommonConfigRead.java
@@ -13,4 +13,108 @@ public class CommonConfigRead {
   private static int PieceSize;
 
 
+
+	/**
+	* Returns value of common_file_name
+	* @return
+	*/
+	public static string getCommon_file_name() {
+		return common_file_name;
+	}
+
+	/**
+	* Returns value of NumberOfPreferredNeighbors
+	* @return
+	*/
+	public static int getNumberOfPreferredNeighbors() {
+		return NumberOfPreferredNeighbors;
+	}
+
+	/**
+	* Sets new value of NumberOfPreferredNeighbors
+	* @param
+	*/
+	public static void setNumberOfPreferredNeighbors(int NumberOfPreferredNeighbors) {
+		CommonConfigRead.NumberOfPreferredNeighbors = NumberOfPreferredNeighbors;
+	}
+
+	/**
+	* Returns value of UnchokingInterval
+	* @return
+	*/
+	public static int getUnchokingInterval() {
+		return UnchokingInterval;
+	}
+
+	/**
+	* Sets new value of UnchokingInterval
+	* @param
+	*/
+	public static void setUnchokingInterval(int UnchokingInterval) {
+		CommonConfigRead.UnchokingInterval = UnchokingInterval;
+	}
+
+	/**
+	* Returns value of OptimisticUnchokingInterval
+	* @return
+	*/
+	public static int getOptimisticUnchokingInterval() {
+		return OptimisticUnchokingInterval;
+	}
+
+	/**
+	* Sets new value of OptimisticUnchokingInterval
+	* @param
+	*/
+	public static void setOptimisticUnchokingInterval(int OptimisticUnchokingInterval) {
+		CommonConfigRead.OptimisticUnchokingInterval = OptimisticUnchokingInterval;
+	}
+
+	/**
+	* Returns value of FileName
+	* @return
+	*/
+	public static string getFileName() {
+		return FileName;
+	}
+
+	/**
+	* Sets new value of FileName
+	* @param
+	*/
+	public static void setFileName(string FileName) {
+		CommonConfigRead.FileName = FileName;
+	}
+
+	/**
+	* Returns value of FileSize
+	* @return
+	*/
+	public static int getFileSize() {
+		return FileSize;
+	}
+
+	/**
+	* Sets new value of FileSize
+	* @param
+	*/
+	public static void setFileSize(int FileSize) {
+		CommonConfigRead.FileSize = FileSize;
+	}
+
+	/**
+	* Returns value of PieceSize
+	* @return
+	*/
+	public static int getPieceSize() {
+		return PieceSize;
+	}
+
+	/**
+	* Sets new value of PieceSize
+	* @param
+	*/
+	public static void setPieceSize(int PieceSize) {
+		CommonConfigRead.PieceSize = PieceSize;
+	}
 }
