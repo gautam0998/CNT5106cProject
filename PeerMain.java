acbfcbf0525ecb01de84434d8a6086c1b217dee7
Shubham <sp.shubham1997@gmail.com>
2021-03-11 01:11:11 +0530

    final Midpoint

--- PeerMain.java
+++ PeerMain.java
@@ -1,19 +1,21 @@
 import java.io.*;
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public class PeerMain {
     public static void main(String[] args) throws Exception {
-        
+
+        int peerID = 1001; //4 bit PeerID       
 
         //FileLocation
         String sentLocation = "./file/sent/input.jpg";        
         
-         //Create A new server socket for this peer/server with port number 7777
-         ServerSocket serverSocket = new ServerSocket(7777);
+        //Create A new server socket for this peer/server with port number 7777
+        ServerSocket serverSocket = new ServerSocket(7777);
 
-         //Connect to the created socket
-         Socket socket= serverSocket.accept();
-
+        //Connect to the created socket
+        Socket socket= serverSocket.accept();
 
         //Provide the file location to be transferred
         File file = new File(sentLocation);
@@ -22,24 +24,29 @@ public class PeerMain {
         int fileSize = (int) file.length();
 
         //try to send a data output stream to share the file size
-        try {
-            DataOutputStream dOutputStream = new DataOutputStream(socket.getOutputStream());
+        try
+         {
+             
             
-            dOutputStream.writeInt(fileSize);
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            
+            dataOutputStream.writeInt(fileSize);
 
             //received message code initialization
-            int receivedMessage;
+            byte []receivedMessage= new byte[32];
+
 
             //received message data input stream
             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
 
-            byte []t = new byte[32];
+            byte []t = new byte[4];
 
             //received message stored in the variable
-            receivedMessage = dataInputStream.read();
+            dataInputStream.read(receivedMessage);
 
+            int serverPeerID = ByteBuffer.wrap(Arrays.copyOfRange(receivedMessage, 28, 32)).getInt();
 
-            System.out.println(receivedMessage);
+            System.out.println(serverPeerID);
             
             //check if the received message code is correct
             if(true){
